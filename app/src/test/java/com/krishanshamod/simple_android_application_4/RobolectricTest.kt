ca67ec36558688b8767ed85f9acbdc8a09be44fe
Krishan Shamod <krishanshamod@gmail.com>
2022-05-07 13:12:44 +0530

    Added Robolectric tests

--- app/src/test/java/com/krishanshamod/simple_android_application_4/RobolectricTest.kt
+++ app/src/test/java/com/krishanshamod/simple_android_application_4/RobolectricTest.kt
@@ -1,6 +1,8 @@
 package com.krishanshamod.simple_android_application_4
 
 import android.widget.TextView
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -15,6 +17,21 @@ class RobolectricTest {
     fun check_activity_text() {
         val activity = Robolectric.buildActivity(Activity::class.java).create().get()
         val textViewActivity : TextView = activity.findViewById(R.id.textViewActivity)
+
         assertEquals("Hello World!", textViewActivity.text)
+    }
+
+    @Test
+    fun check_fragment_text(){
+        val activity = Robolectric.buildActivity(Activity::class.java).create().get()
+        val fragment = Fragment()
+
+        val fragmentManager: FragmentManager = activity.supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.add(fragment, null)
+        fragmentTransaction.commit()
+
+        val textViewFragment : TextView = activity.findViewById(R.id.textViewFragment)
+        assertEquals("Hello World!", textViewFragment.text)
     }
 }
