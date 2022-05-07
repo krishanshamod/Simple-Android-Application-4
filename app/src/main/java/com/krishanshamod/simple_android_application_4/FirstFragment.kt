65979699e953859d12022d12629fe5bd8c303aca
Krishan Shamod <krishanshamod@gmail.com>
2022-05-07 12:20:45 +0530

    Removed unnecessary codes and files which added from generating the sample project

--- app/src/main/java/com/krishanshamod/simple_android_application_4/FirstFragment.kt
+++ app/src/main/java/com/krishanshamod/simple_android_application_4/FirstFragment.kt
@@ -5,18 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import com.krishanshamod.simple_android_application_4.databinding.FragmentFirstBinding
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
 class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -32,9 +25,6 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
     }
 
     override fun onDestroyView() {
